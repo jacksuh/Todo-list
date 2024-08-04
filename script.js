bb51ad349ec5331b27f27ecdcf05d68402549558
jacksuh <jacksonsdss@gmail.com>
2024-08-04 01:15:23 -0300

    Implementando novas funcionalidades

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskButton = document.getElementById('addTaskButton');
     const taskList = document.getElementById('taskList');
 
+    setInterval(updateTime, 1000);
+
+    function updateTime() {
+        const now = new Date();
+        const formattedDateTime = formatDateTime(now);
+        timeDisplay.textContent = formattedDateTime;
+    }
+
+    function formatDateTime(dateTime) {
+        const day = String(dateTime.getDate()).padStart(2, '0');
+        const month = String(dateTime.getMonth() + 1).padStart(2, '0'); // Janeiro é 0
+        const year = dateTime.getFullYear();
+        const hours = String(dateTime.getHours()).padStart(2, '0');
+        const minutes = String(dateTime.getMinutes()).padStart(2, '0');
+        const seconds = String(dateTime.getSeconds()).padStart(2, '0');
+        return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+    }
+
     // Carregar tarefas do localStorage ao carregar a página
     loadTasks();
 
